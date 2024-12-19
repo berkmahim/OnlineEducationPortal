bb970526c46d25c0352c2b91a4598fdf263e887a
berkmahim <berkmahim620@gmail.com>
2024-12-19 14:30:54 +0300

    Entities created

---
+++ EduPortal.Entity/Entities/BlogCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPortal.Entity.Entities
+{
+    public class BlogCategory
+    {
+        public int BlogCategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ EduPortal.Entity/Entities/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduPortal.Entity.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CouseName { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public CourseCategory Category { get; set; }
+        public decimal Price { get; set; }
+        public bool IsShown { get; set; }
+
+    }
+}
